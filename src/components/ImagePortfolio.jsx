157dd488b5ef4e6284c7c50b4fe1071b2e0715a6
aswinss18 <aswinartography18@gmail.com>
2024-09-30 08:06:56 +0530

    Basic structure of Portfolio Gallery is created

---
+++ src/components/ImagePortfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ImagePortfolio() {
+  return (
+    <div className="w-[9rem] border-2 border-red-600 rounded-3xl overflow-hidden">
+      <img src="/public/images/Avatar.jpeg" alt="" />
+    </div>
+  );
+}
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,19 +1,34 @@
 import React from "react";
+import ImagePortfolio from "./ImagePortfolio";
+import { useContext } from "react";
+import { MyContext } from "./AppLayout";
 
 export default function Portfolio() {
+  const isLightMode = useContext(MyContext);
+
   return (
-    <div className="">
+    <div className="w-[100vw] h-fit mt-[5rem] flex flex-col items-center  ">
       <div>
-        <h1>My Works</h1>
+        <h1 className="text-2xl font-bold">My Works</h1>
       </div>
-      <div>
-        <span>All</span>
-        <span>Web</span>
+      <div className="">
+        <span className="">All</span>
+        <span
+          className={`font-semibold px-1 border-b-[2.5px]  transition-all duration-200 ease-in-out  transform hover:border-b-[2.5px] ${
+            isLightMode ? `hover:border-gray-950` : `hover:border-white`
+          } `}
+        >
+          Web
+        </span>
         <span>App</span>
         <span>Graphic</span>
         <span>UI UX</span>
       </div>
-      <div className=""></div>
+      <div className="grid grid-cols-3 px-5 py-5  gap-4 ">
+        <ImagePortfolio /> <ImagePortfolio /> <ImagePortfolio />{" "}
+        <ImagePortfolio /> <ImagePortfolio /> <ImagePortfolio />{" "}
+        <ImagePortfolio /> <ImagePortfolio /> <ImagePortfolio />
+      </div>
     </div>
   );
 }
