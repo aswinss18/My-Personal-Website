847043ae5d67456025c2b2a956bd8273797c2efa
aswinss18 <aswinartography18@gmail.com>
2024-09-30 21:19:40 +0530

    Menu bar div is created and background is blurred.

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -5,8 +5,14 @@ export default function Menubar() {
   const { isLightMode } = useContext(MyContext);
 
   return (
-    <div className="absolute z-10  w-full h-full backdrop-blur-sm hidden">
-      <div className="w-[60vw] h-[100vh] rounded-r-3xl">
+    <div className="absolute z-10  w-full h-full backdrop-blur-sm ">
+      <div
+        className={`w-[60vw] h-[100vh] border-r-4 border-y-4 rounded-r-3xl ${
+          isLightMode
+            ? `bg-white border-gray-950 text-gray-950`
+            : `bg-gray-950 border-white text-white`
+        }`}
+      >
         <h2 className="font-san font-bold text-[1.5rem]  element ml-1">
           Aswin
         </h2>
